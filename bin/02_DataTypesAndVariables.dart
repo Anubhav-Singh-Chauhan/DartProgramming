dc76a16cd05c9ad0c3576750d826d5c538e639f3
Anubhav-Singh-Chauhan <chauhananubhav0011@gmail.com>
2022-07-25 17:16:14 +0530

    DataTypesAndVariables

---
+++ bin/02_DataTypesAndVariables.dart
@@ -0,0 +1,14 @@
+void main(){
+  //All data types in Dart are objects,
+  //therefore, values are by default null.
+  int age = 10;
+  var a = 10;  // it is inferred as integer automatically
+
+
+  String name = "harry";
+  var str = "harry";
+
+  print(age  + a);
+  print(name + " " + str);
+
+}
